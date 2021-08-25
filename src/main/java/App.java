53e9fc96488d456b43f54fd3d6e4db3bc5f90441
VinnieIT <mwangivin3@gmail.com>
2021-08-25 12:38:27 +0300

    Update Interaction

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,7 +22,11 @@ public class App {
                 Scanner encryptInput = new Scanner(System.in);
                 System.out.println("Enter the text you want to encrypt");
                 String userInput = encryptInput.nextLine();
-
+                Scanner eKeyInput = new Scanner(System.in);
+                System.out.println("Enter the Key you want to encrypt with");
+                String eKeyString = eKeyInput.nextLine();
+                int eKeyControl = Integer.parseInt(eKeyString);
+                System.out.println(newEncryption.myEncrypt(userInput, eKeyControl));
 
             } else if (userChoice.toUpperCase().equals("D")) {
                 decrypt newEncryption = new decrypt();
